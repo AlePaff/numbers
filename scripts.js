8739f27f0eaa57e518c242555517895f1c59f404
AlePaff <43119891+AlePaff@users.noreply.github.com>
2022-08-07 02:11:08 -0300

    prototipo funcionado
    toma un dato de wolfram y lo muestra en pantalla

--- scripts.js
+++ scripts.js
@@ -1,15 +1,19 @@
 var api_id = "XLPLQ9-WVTJ4GL8WU";
-
 var link = "http://api.wolframalpha.com/v2/query?appid=" + api_id;
-// https://www.youtube.com/watch?v=uxf0--uiX0I
+// https://www.youtube.com/watch?v=uxf0--uiX0I  -> como usar una api
+
+
+var input_numero = '16.09';
+var formas_cerradas = 'https://api.wolframalpha.com/v2/query?input=closed+forms+of+' + input_numero + '&format=plaintext&output=JSON&appid=' + api_id;
+
 
 const api_url = 'https://api.wheretheiss.at/v1/satellites/25544';
 async function getISS(){
-	const response = await fetch(api_url);
+	const response = await fetch(formas_cerradas);
 	const data = await response.json();
-	const {latitude, longitud} = data;
 
-	document.getElementById('lat').textContent = latitude;
+	document.getElementById('formas_cerradas').innerHTML = data.queryresult.pods[1].subpods[2].plaintext;;
+
 
 }
 
