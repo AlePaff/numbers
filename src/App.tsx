29d35c3d651f17bf3af4d388a7bbb03c3ac9a1d0
AlePaff <alejandropaff@gmail.com>
2023-03-04 00:36:55 -0300

    useForm y luego useQuery funciona pero hay que apretar primero el boton de form y luego el de query

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,24 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 import GetData from './components/GetData'
 import { useForm } from "react-hook-form";
-
-
-function handleClick() {
-  alert("hola");
-}
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from "react-query/devtools";
 
 function App() {
+  return (
+    // para permitir usar el hook useQuery globalmente (en cualquier componente)
+    <QueryClientProvider client={new QueryClient()}>
+      <Wolframe />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  )
+}
+
+
+function Wolframe() {
   const { register, handleSubmit, formState: { errors } } = useForm();
   const [data, setData] = useState({});
 
-  useEffect(() => {
-    console.log(data.input_user)
-  }, [data.input_user])
 
   return (
     <>
@@ -28,9 +33,9 @@ function App() {
 
         <form onSubmit={handleSubmit((data) => setData(data))}>
           {/*
-              setData toma el json que devuelve handleSubmit y lo guarda en el state cuando se hace submit
-              register es una funcion que guarda el valor del input en el state
-          */}
+                setData toma el json que devuelve handleSubmit y lo guarda en el state cuando se hace submit
+                register es una funcion que guarda el valor del input en el state
+            */}
           <input {...register(
             "input_user",
             // { pattern: /^[0-9]+.+$/i }
@@ -39,14 +44,16 @@ function App() {
             placeholder="Write here a number (eg. 16.09)"
             className="w-80 mx-auto border-2 rounded-l-md p-2 my-5 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" />
           {/* {errors?.input_user?.type === "pattern" && (
-            <p>Alphabetical characters only</p>
-          )} */}
+              <p>Alphabetical characters only</p>
+            )} */}
           <button type="submit" className="bg-purple-600 text-white rounded-r-md px-3 h-12">=</button>
         </form>
 
-        {/* cuando se ejecute el submit recien ahi usa GetData */}
-        {data.input_user && <GetData input_numero={data.input_user} />}
+          {/* <div>{data.input_user}</div> */}
 
+        {/* cada vez que se aprete el boton llama al componente GetData */}
+        {data.input_user && <GetData input_numero={data.input_user} />}
+        {/* <GetData input_numero={data.input_user} /> */}
 
 
 
@@ -68,9 +75,9 @@ function App() {
 
 
       {/* closed form https://en.wikipedia.org/wiki/Closed-form_expression 
-      a closed-form expression is a mathematical expression that uses a finite number of standard operations.
-      It may contain constants, variables, operations and functions (eg. closed form of 16.09 => e^2pilog(2) = 16.090305)
-      */}
+        a closed-form expression is a mathematical expression that uses a finite number of standard operations.
+        It may contain constants, variables, operations and functions (eg. closed form of 16.09 => e^2pilog(2) = 16.090305)
+        */}
 
 
 
@@ -78,6 +85,7 @@ function App() {
 
     </>
   )
+
 }
 
 export default App
--- src/components/GetData.tsx
+++ src/components/GetData.tsx
@@ -1,55 +1,63 @@
 import React, { useState, useEffect } from 'react';
+import { useQuery } from "react-query";
+
 
 
 //from https://reactjs.org/docs/faq-ajax.html
-function GetData(props: any) {
+function GetData({ input_numero }) {
+
   var api_id = "XLPLQ9-WVTJ4GL8WU";
   var link = "http://api.wolframalpha.com/v2/query?appid=" + api_id;
-  
-  var formas_cerradas = 'https://api.wolframalpha.com/v2/query?input=closed+forms+of+' + props.input_numero + '&format=plaintext&output=JSON&appid=' + api_id;
-  
+
+  var formas_cerradas = 'https://api.wolframalpha.com/v2/query?input=closed+forms+of+' + input_numero + '&format=plaintext&output=JSON&appid=' + api_id;
+
+  console.log(input_numero)
   console.log(formas_cerradas)
 
+  // funcion que hace el fetch
+  const getData = async () => {   //acepta cero argumentos
+    const response = await fetch(formas_cerradas);    //fetch devuelve una promesa
+    const data = await response.json();     //la promesa se resuelve con un json (que a su vez devuelve una promesa)
+    return data;
+  };
 
-  const [error, setError] = useState(null);
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [items, setItems] = useState([]);
 
-  // Note: the empty deps array [] means this useEffect will run once similar to componentDidMount()
-  useEffect(() => {
-    // fetch("https://jsonplaceholder.typicode.com/users")
-    fetch(formas_cerradas)
-      .then(res => res.json())    //convierte la respuesta a json
-      .then((result) => {       //result es el json
-        setIsLoaded(true);      //cambia el estado de isLoaded a true
-        setItems(result);       //cambia el estado de items a lo que devuelve la api
-      },
-        // Note: it's important to handle errors here instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
-        (error) => {
-          setIsLoaded(true);      //cambia el estado de isLoaded a true
-          setError(error);      //cambia el estado de error a lo que devuelve la api
-        }
-      )
-  }, [])
+  //uso del hook useQuery
+  const { status, data, error, isFetching, refetch } = useQuery(["wolfram_key"], getData, {
+    refetchOnWindowFocus: false,                 //Hace que no se haga fetch cuando se cambia de pestaña
+    enabled: false,    // no hacer fetch automaticamente (tampoco se actualiza la petición al apretar el boton, debe ser manual)
+    // refetchOnMount: true
+  });
 
-  console.log(formas_cerradas)
 
-  //chequeos en base al response de la api
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  } else if (!isLoaded) {
-    return <div>Loading...</div>;
-  } else {
-    return (
-      <div>
-        
-        {console.log(items.queryresult)}
-        {/* {items.queryresult.pods[0].title} */}
-        
-      </div>
-    );
-  }
+  console.log(data?.queryresult);
+  // return;
+
+  // console.log(data?.queryresult);
+  // console.log(data.queryresult.pods[1]);
+  // console.log(data.queryresult.pods[1].subpods[0]);
+  // console.log(data.queryresult.pods[1].subpods[0].plaintext);
+
+
+  // const handleClick = () => {
+  //   // (!) manually refetch on click event
+  //   refetch();
+  // };
+
+  if (status === 'loading') { return <div>Loading...</div>; }
+  if (isFetching) { return <div>Fetching...</div>; }
+  if (status === 'error') { return <div>Error: {error.message}</div>; }
+  // if(status === 'success') {return <div>{data}</div>;}
+  // return
+
+  return (
+    <div>
+      <button onClick={refetch}>Fetch data</button>
+      <h1>Formas cerradas</h1>
+      {/* <p>{data.queryresult.pods[1].subpods[0].plaintext}</p> */}
+    </div>
+  )
+
 
 }
 
