98893174b49dbe4dbbdddb09f38c71b418a73e7c
AlePaff <alejandropaff@gmail.com>
2023-03-07 01:25:30 -0300

    search de ids terminada

--- src/App.tsx
+++ src/App.tsx
@@ -14,12 +14,25 @@ function App() {
 }
 
 
-function Wolframe() {
+//por ejemplo 4356 -> 4 + 3 + 5 + 8 = 20 -> 2 + 0 = 2
+function raizDigital(numero_entero) {
+  const numero = numero_entero.toString();
+  if (numero.length === 1) {
+    return numero;
+  }
+  const suma_digitos = numero.split('').reduce((a, b) => parseInt(a) + parseInt(b));
+  console.log(suma_digitos)
+  return raizDigital(suma_digitos);
+}
 
+
+function Wolframe() {
   const more_closed_forms = "&podstate=PossibleClosedForm__More"
   const images = "&format=image"    //sin necesidad de latex ni nada
   const api_id = "XLPLQ9-WVTJ4GL8WU"
-  const calendarios = "&podstate=RomanNumerals__Other+historical+numerals"
+  const roman = "&podstate=RomanNumerals__Other+historical+numerals"
+  const calendarios = "&podstate=SingleDateFormats__More+formats/calendars"
+  const comparationNumber = 5000000;
 
 
   const [inputValue, setInputValue] = useState('');
@@ -28,41 +41,64 @@ function Wolframe() {
   };
 
 
+  //no hace falta el chequeo de si es entero, decimal o fecha, ya que filter
+  //si no lo encuentra devuelve un array vacío, y map y flatMap sobre un array vacío no generan problemas
+  const getSubpods = (wolframe_query, pod_id) => {
+    let result = wolframe_query?.pods.filter(p => p.id === pod_id).flatMap(pod => pod.subpods).map(subpod => subpod.img);
+    return result
+  }
 
   const parseQuery = (wolframe_output) => {
     console.log("pods ", wolframe_output?.pods)
-    // const numero = parseInt(inputValue, 10);    //base 10
+    const numero = parseFloat(inputValue)
 
 
-    //si es numero entero
-    if (inputValue.match(/^\d+$/)) {
 
-      const filteredPods = wolframe_output?.pods.filter(p => p.id === "Property");// || p.id === "PrimeFactorization");
-      const subpods = filteredPods.flatMap(pod => pod.subpods);   //convierte un array de arrays en un array plano
-      // const plaintexts = subpods.map(subpod => subpod.plaintext);
-      // console.log("plaintexts ", plaintexts)
-      const images = subpods.map(subpod => subpod.img);
+    const propsSubpods = getSubpods(wolframe_output, "Property")
+    const primesSubpods = getSubpods(wolframe_output, "PrimeFactorization")
+    const comparitionSubpods = getSubpods(wolframe_output, "Comparison")
+    const closedFormsSubpods = getSubpods(wolframe_output, "PossibleClosedForm")
+    const calendarSubpods = getSubpods(wolframe_output, "RomanNumerals")
+    const dateSubpods = getSubpods(wolframe_output, "SingleDateFormats")
+    const timeSubpods = getSubpods(wolframe_output, "DifferenceConversions")
 
-      
-      return (
-        <div>
-          <h1>Propiedades</h1>
-          {images.map((image, index) => <img key={index} src={image.src} alt={image.alt} />)}
-          {/* <p>{plaintexts}</p> */}
-        </div>
-      )
-    }
+
+    return (
+      <div>
+        <h2>Propiedades</h2>
+        {propsSubpods.map((image, index) => <img key={index} src={image.src} alt={image.alt} />)}
+
+        <h2>Factorizacion de primos</h2>
+        {primesSubpods.map((image, index) => <img key={index} src={image.src} alt={image.alt} />)}
+
+        <h2>Calendarios</h2>
+        {calendarSubpods.map((image, index) => <img key={index} src={image.src} alt={image.alt} />)}
+
+        <h2>Comparacion</h2>
+        {comparitionSubpods.map((image, index) => <img key={index} src={image.src} alt={image.alt} />)}
+
+        <h2>closedFormsSubpods</h2>
+        {closedFormsSubpods.map((image, index) => <img key={index} src={image.src} alt={image.alt} />)}
+
+        <h2>Fecha</h2>
+        {dateSubpods.map((image, index) => <img key={index} src={image.src} alt={image.alt} />)}
+
+        <h2>Hora</h2>
+        {timeSubpods.map((image, index) => <img key={index} src={image.src} alt={image.alt} />)}
+
+      </div>
+    )
+    // }
 
   }
 
 
 
   const query = useQuery('wolfram', async () => {
-    let api = `https://api.wolframalpha.com/v2/query?input=${inputValue}&output=JSON&appid=${api_id}${calendarios}${more_closed_forms}`
+    let api = `https://api.wolframalpha.com/v2/query?input=${inputValue}&output=JSON&appid=${api_id}${calendarios}${more_closed_forms}${roman}`
     const response = await fetch(api);
     const data = await response.json();     //await se usa para esperar a que la promesa se resuelva
     console.log("los datos de la query fueron ", data.queryresult)
-
     const resultados = parseQuery(data.queryresult)
     return resultados;
   }, {
@@ -93,9 +129,8 @@ function Wolframe() {
         <button
           className="bg-purple-600 text-white rounded-r-md px-3 h-12"
           onClick={handleClick}
-        >Buscar</button>
-        {query.isLoading && <div>Cargando...</div>}
-        {query.isFetching && <div>Buscando...</div>}
+        >Search</button>
+        {(query.isLoading || query.isFetching) && <div>Loading...</div>}
         {query.isSuccess && <div>{query.data}</div>}
         {query.isError && <div>Error: {query.error.message}</div>}
       </div>
